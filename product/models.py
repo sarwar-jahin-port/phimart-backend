67bd6660df6caa55bc761bc6ecfad1c6f57c3be0
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-04-12 14:07:58 +0600

    db connected successfully

--- product/models.py
+++ product/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
-from product.validators import validate_file_size
-
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -32,11 +31,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(
-        upload_to="products/images/", validators=[validate_file_size])
-    # file = models.FileField(upload_to="product/files",
-    #                         validators=FileExtensionValidator(['pdf']))
-
+    image = CloudinaryField('image')
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
